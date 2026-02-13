e4361afc4d7e1f38ee2c6140ad40f28201911998
titomizo0o0o-del <titomizo0o0o@gmail.com>
2026-02-13 22:18:29 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Personal Love Messages
-const messageElement = document.getElementById("love-message"); = [
+const loveMessages = [
     "حبيبتقلب طارق من جوااا🥹\nبحبك اوي يعيوني ربنا يخليكي ليا وارجع حبيبك تاني وارجع وطروقك وتوتي تاني🥹\nان مش نفضل طول العمر سوا يعيونيي انتتت واشوفك كويسه وفاحسن حته في الدنيا كدههههووو وأفضل فخور بيكي موتتتت طول الوقتتتتت\nبحبك اوي والله 🥹🥹🥹🥹🥹🥹",
     "U mean more to me than I ever know how to explain. U're my comfort, my favorite smile, and the one my heart always chooses, I don't need special days to love you. You're always my choice, today and every day."
 ];
+
 
 const photoLinks = [
     "img1.jpg",
@@ -12,6 +12,40 @@ const photoLinks = [
     "img5.jpg"
 ];
 
-// This is a placeholder script
-console.log(loveMessages);
-console.log(photoLinks);
+let currentMessageIndex = 0;
+let currentPhotoIndex = 0;
+
+// وظيفة تغيير الرسائل
+function nextMessage() {
+    currentMessageIndex = (currentMessageIndex + 1) % loveMessages.length;
+    document.getElementById("love-message").innerText = loveMessages[currentMessageIndex];
+}
+
+// وظائف تغيير الصور
+function nextPhoto() {
+    currentPhotoIndex = (currentPhotoIndex + 1) % photoLinks.length;
+    document.getElementById("gallery-image").src = photoLinks[currentPhotoIndex];
+}
+
+function prevPhoto() {
+    currentPhotoIndex = (currentPhotoIndex - 1 + photoLinks.length) % photoLinks.length;
+    document.getElementById("gallery-image").src = photoLinks[currentPhotoIndex];
+}
+
+// وظائف الأغنية
+const audio = document.getElementById("audio-player");
+function playMusic() { audio.play(); }
+function pauseMusic() { audio.pause(); }
+
+// تشغيل أول حاجة أول ما الموقع يفتح
+window.onload = () => {
+    document.getElementById("love-message").innerText = loveMessages[0];
+    document.getElementById("gallery-image").src = photoLinks[0];
+    
+    // ربط الزراير بالوظائف
+    document.querySelector(".next-message-btn").onclick = nextMessage;
+    document.getElementById("next-btn").onclick = nextPhoto;
+    document.getElementById("prev-btn").onclick = prevPhoto;
+    document.getElementById("play-btn").onclick = playMusic;
+    document.getElementById("pause-btn").onclick = pauseMusic;
+};
